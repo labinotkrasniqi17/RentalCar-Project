fe9c2a9e84934fe8f285d70775a4ff9e5edffff1
Labinot <labinot.krasniqi@ubt-uni.net>
2022-06-14 00:52:49 +0200

    Krijimi i modelit Car

---
+++ RentalCar/RentalCar/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace RentalCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+
+        //public DateTime StartDate { get; set; }
+
+        //public DateTime EndDate { get; set; }
+
+        //public CarCategory CarCategory { get; set; }
+
+
+
+
+    }
+}
