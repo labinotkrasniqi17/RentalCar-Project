5604ab3cfa8dba1e2bd1541170eeeb00c3a70adf
Labinot <labinot.krasniqi@ubt-uni.net>
2022-06-26 14:07:33 +0200

    Shtimi i modelit Contact

---
+++ RentalCar/RentalCar/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace RentalCar.Models
+{
+  
+    public class Contact
+    {
+    
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
