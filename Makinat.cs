e492bf0a710a03bbdc1e726d0ad83929e49b95f8
Eldi Vuniqi <ev52271@ubt-uni.net>
2022-07-01 17:45:33 +0200

    new commit

---
+++ Makinat.cs
@@ -0,0 +1,11 @@
+class Makinat{ 
+    public string lloji; 
+    public int cmimi; 
+    public boolean disponueshmeria; 
+
+    public bill(string lloji, int cmimi, boolean disponueshmeria) { 
+        lloji= this.lloji; 
+        cmimi= this.cmimi; 
+        disponueshmeria = this.disponueshmeria;
+    }
+}
