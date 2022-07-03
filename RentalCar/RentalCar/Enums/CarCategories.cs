fe65d82dcd0d45978cc3978e04436cf50bcd9070
Lir07 <93524804+Lir07@users.noreply.github.com>
2022-07-03 23:57:44 +0200

    added enums

---
+++ RentalCar/RentalCar/Enums/CarCategories.cs
@@ -0,0 +1,16 @@
+﻿namespace RentalCar.Models
+{
+    public enum CarCategories
+    {
+        SUV,
+        Hatchback,
+        Crossover,
+        Convertible,
+        Sedan,
+        Sport,
+        Coupe,
+        Minivan,
+        PickupTruck,
+        StationWagon
+    }
+}
