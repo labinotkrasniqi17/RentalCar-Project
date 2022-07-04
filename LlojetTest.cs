8edf8010812446fe51fbab95a87e50483bca47a0
Eldi Vuniqi <ev52271@ubt-uni.net>
2022-07-04 02:40:32 +0200

    Llojet e Makinave Test

---
+++ LlojetTest.cs
@@ -0,0 +1,9 @@
+class LlojetTest { 
+    static void Main(string[] args) { 
+        string [] strArray = new string[] {"Volkswagen", "BMw", "Mercedes-Benz", "Toyota", "Audi",}; 
+        foreach (string str in strArray)
+        Console.WriteLine(str); 
+
+        Console.ReadKey();
+    }
+}
