a64cf8e41897fa6cb4002e5ce911a00927c6d855
Eldi Vuniqi <ev52271@ubt-uni.net>
2022-07-14 18:36:09 +0200

    faq

---
+++ faq.cs
@@ -0,0 +1,9 @@
+class faq { 
+     static void main(string[] args) { 
+        string [] first = new string[] {"Why should I use this car?"};
+        string [] second = new string[] {"Why should I use this rent a car agency?"};
+        string [] third = new string[] {"How old do I need to be to rent a car?"};
+        string [] fourth = new string[] {"Can I book a one-way car rental?"};
+        string [] fifth = new string[] {"How do I find the cheapest car for rent"};
+     }
+}
