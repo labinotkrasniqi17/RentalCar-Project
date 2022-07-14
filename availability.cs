635ab07686b1c9d97b5d0e5cf9e7a5c2c19014c8
Altinii <102525876+Altinii@users.noreply.github.com>
2022-07-14 18:22:51 +0200

    Availability of the cars.

---
+++ availability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace availability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string emrimakines;
+            int numrimakinave;
+            double cmimimakines;
+            bool availability;
+
+        }
+    }
+}
